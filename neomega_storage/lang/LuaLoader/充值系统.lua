bc7473a2c93c9b97bf0df5b79b05670ad6be3057
potatoblock <potatoblock@163.com>
2024-09-30 16:48:35 +0800

    Update 充值系统.lua

--- neomega_storage/lang/LuaLoader/充值系统.lua
+++ neomega_storage/lang/LuaLoader/充值系统.lua
@@ -18,7 +18,7 @@ print("config of 充值系统:  ",json.encode(coromega.config))
 coromega:when_called_by_terminal_menu({
     triggers = { "充值系统" },
     argument_hint = "[arg1] [arg2] ...",
-    usage = "对接爱发电的创造系统,用于现金充值.",
+    usage = "对接爱发电的充值系统,用于现金充值.",
 }):start_new(function(input)
     print("hello from 充值系统!")
 end)
@@ -26,7 +26,7 @@ end)
 coromega:when_called_by_game_menu({
     triggers = { "充值兑换" , "token" },
     argument_hint = "",
-    usage = "对接爱发电的创造系统,用于现金充值.",
+    usage = "对接爱发电的充值系统,用于现金充值.",
 })
 :start_new(function(chat)
     local caller_name = chat.name
@@ -34,15 +34,17 @@ coromega:when_called_by_game_menu({
     local input = chat.msg
     local mode = caller:ask("请输入你要进行的操作的编号:\n1.购买\n2.兑换")
     if mode == "1" then
-        caller:say("§6好的,请用任意软件扫描地上的地图画.\n如果手持地图后不显示,您可以试一试在设置-视频中关闭隐藏手.\n扫描完成后重新启动该菜单,并选择兑换.")
+        caller:say("§6好的,请用任意软件扫描地上的地图画.\n如果手持地图后不显示,您可以试一试在设置-视频中关闭隐藏手.\n扫描完成并购买后重新启动该菜单,并选择兑换.")
         local QR = coromega.config["充值码结构名"]
         coromega:send_ws_cmd(("execute as %s at @s run structure load %s ~~~"):format(caller_name, QR), false)
     elseif mode == "2" then
         local token = caller:ask("请输入兑换码.\n如果还没有,请重新启动本菜单项,并选择购买选项.")
         local function IsInTable(value, tbl)
             for k, v in ipairs(tbl) do
-                if v == value then
-                    tbl[k] = nil
+                if v == "占位" then
+                    return {false}
+                elseif v == value then
+                    tbl[k] = "占位"
                     return {true, tbl}
                 end
             end
@@ -52,6 +54,14 @@ coromega:when_called_by_game_menu({
         db:set("占位", "null")
         local rmb_5 = db:get("RMB5")
         local rmb_10 = db:get("RMB10")
+        if rmb_5 == nil then
+            db:set("RMB5", {})
+            local rmb_5 = {}
+        end
+        if rmb_10 == nil then
+            db:set("RMB10", {})
+            local rmb_10 = {}
+        end
         local in_table_5 = IsInTable(token, rmb_5)
         local in_table_10 = IsInTable(token, rmb_10)
         if in_table_5[1] == true then
